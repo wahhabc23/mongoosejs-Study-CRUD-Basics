70410ba1a98edca326ff85325627bedd4224dbe0
wahhabc23 <wahhabc23@gmail.com>
2021-08-29 22:59:51 +0530

    Few More Commented Out 22:59/29/08/2021

--- app.js
+++ app.js
@@ -2,37 +2,62 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/gamesDB");
 
-
-
 const Game = mongoose.model('Game', {
-  name : String , os : String , cds: Number
+  name: String,
+  os: String,
+  cds: Number
 });
 
-const Owner = mongoose.model("Owner",{
-  name: String , ph: Number
+const Owner = mongoose.model("Owner", {
+  name: String,
+  ph: Number
 });
 
-// const game = new Game({ name: "GTA V" ,os : "Win7 and above", cds: 5 });
+// const game = new Game({
+//   name: "GTA V",
+//   os: "Win7 and above",
+//   cds: 5
+// });
 // const game2 = new Game({
-//   name: "Watch Dogs" , os : "Win7 and above" , cds: 4
-// })
-
-// game.save().then(() => console.log('Successfully Added'));
-// game2.save().then(() => console.log("Successfully Added"));
-
-const farcry = new Game({
-  name: "Farcry" , os : "XP and above" , cds: 2
+//   name: "Watch Dogs",
+//   os: "Win7 and above",
+//   cds: 4
+// });
+//
+//
+// const farcry = new Game({
+//   name: "Farcry",
+//   os: "XP and above",
+//   cds: 2
+// });
+//
+// const GTA = new Owner({
+//   name: "Wahhab",
+//   ph: 9876543210
+// });
+//
+// const WD = new Owner({
+//   name: "Rock",
+//   ph: 9874125630
+// });
+//
+// game.save();
+// game2.save();
+// farcry.save();
+// GTA.save();
+// WD.save();
+// console.log('Successfully Added');
+const FF = new Game({
+  name: "Free Fire",os:"Android or iOS"
+});
+const PUBG = new Game({
+  name:"Player Unknown's Battle Ground",os:"Android or iOS or Windows" ,cds:2
 });
 
-const GTA = new Owner({
-  name: "Wahhab" , ph: 9876543210
+Game.insertMany([FF , PUBG ],function(err){
+  if(err){
+    console.log('Failed');
+  }else{
+    console.log('Added');
+  };
 });
-
-const WD = new Owner({
-  name: "Rock" , ph: 9874125630
-});
-
-farcry.save();
-GTA.save();
-WD.save();
-console.log('Successfully Added');
